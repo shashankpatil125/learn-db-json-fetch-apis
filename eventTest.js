fc20e25f57ef4be43d4423e06a6a5993ce3af33c
shashank lalit patil <shashankpatil125@gmail.com>
2022-08-22 06:23:05 +0000

    events

---
+++ eventTest.js
@@ -0,0 +1,10 @@
+const Event=require('events')
+
+const fire=new Event();
+
+fire.on('firefunction', (name)=>{
+    console.log("hello world! "+name + " max")
+})
+
+fire.emit('firefunction','shashank')
+
--- filehandle.js
+++ filehandle.js
@@ -1,2 +1,9 @@
 const fs=require('fs');
 fs.writeFileSync("tf/tex.txt","hello");
+var read = fs.readFileSync("tf/tex.txt");
+
+var ord=read.toString()
+console.log(ord);
+
+module.exports.ord=ord;
+
